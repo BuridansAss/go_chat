acc9754d424feefc7fc899ebd1700da5d2ff10d3
Evgeniy_Khorsun <e.horsun@playneta.gg>
2019-09-25 20:36:22 +0300

    set postgres

--- main.go
+++ main.go
@@ -1,9 +1,5 @@
 package main
 
-import (
-	"buridansAss/chat/src/http"
-)
-
 func main() {
-	http.New()
+
 }
---
+++ src/providers/config.go
@@ -0,0 +1,22 @@
+package providers
+
+type Config struct {
+	DBUser     string
+	DBPass     string
+	DBName     string
+	ListenPort string
+}
+
+func NewConfig() *Config {
+	dbUser := "root"
+	dbPass := "123"
+	dbName := "go_chat"
+	port := "9876"
+
+	return &Config{
+		DBUser:     dbUser,
+		DBPass:     dbPass,
+		DBName:     dbName,
+		ListenPort: port,
+	}
+}
